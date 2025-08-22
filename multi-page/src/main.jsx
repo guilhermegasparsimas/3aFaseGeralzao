b9f48859e70ba4237974350fd271e415aaccbc77
guilhermegasparsimas <guisimas0201@gmail.com>
2025-08-21 21:31:05 -0300

    fundamentos atz

--- multi-page/src/main.jsx
+++ multi-page/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+// REACT ROUTER
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Hello World</h2>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
