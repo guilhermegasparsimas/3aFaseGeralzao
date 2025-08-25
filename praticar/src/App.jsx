3bddc9381d066245aefaaa7d7663e9ba5ad428f2
guilhermegasparsimas <guisimas0201@gmail.com>
2025-08-25 20:41:50 -0300

    res

--- praticar/src/App.jsx
+++ praticar/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Soma from './components/Soma/Soma'
+import Minuto from './components/Minuto/Minuto'
 
 function App() {
 
   return (
     <>
-      <Soma />
+      {/* <Soma /> */}
+      <Minuto />
     </>
   )
 }
--- praticar/src/components/Minuto/Minuto.jsx
+++ praticar/src/components/Minuto/Minuto.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Minuto = () => {
     const [int, setInt] = useState(0)
     const [seg, setSeg] = useState(0)
 
+    function Converter (){
+      let minutos = Number(int)
+      let conversao = minutos * 60
+      setSeg(conversao)
+    }
   return (
     <>
     <h1>Converter minutos em segundos!</h1>
-    <div>
+    <div className='div-soma' >
         <label>Quantos minutos você quer converter?</label>
         <input type="text" placeholder='minutos...' onChange={(e)=> setInt(e.target.value)} />
         <button onClick={Converter}>Converter</button>
     </div>
+    Minutos: {int} <p></p> Segundos: {seg}
     </>
 )
 }
