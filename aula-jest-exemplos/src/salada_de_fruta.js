082c8cdbc82885d5d8b7db29d46a484f59e1b1cc
guilhermegasparsimas <guisimas0201@gmail.com>
2025-08-25 21:14:00 -0300

    'jest1'

--- aula-jest-exemplos/src/salada_de_fruta.js
+++
@@ -1,8 +0,0 @@
-function salada_de_fruta(){
-    const array = []
-    console.log(array)
-    // array.push("açaí", "cupuaçu")
-    array.push([{aluno: "1",nome: "Guilherme"}])
-    console.log(array)
-}
-salada_de_fruta()
