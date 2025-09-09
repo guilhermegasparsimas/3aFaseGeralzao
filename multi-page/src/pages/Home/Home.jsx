738df976272108630bb7c8153c8e1ff136ed34bd
guilhermegasparsimas <guisimas0201@gmail.com>
2025-09-09 19:14:21 -0300

    as

--- multi-page/src/pages/Home/Home.jsx
+++ multi-page/src/pages/Home/Home.jsx
@@ -5,9 +5,8 @@ const Home = () => {
   return (
     <>
         <Header />
-        {/* <h1>Página Home</h1>
+        <h1>Página Home</h1>
         <p></p>
-        <h2>Hello</h2> */}
     </>
   )
 }
