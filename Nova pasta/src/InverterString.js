f3b8b7acaf8744d4dcd872ad74dc2b1714812323
guilhermegasparsimas <guisimas0201@gmail.com>
2025-09-24 21:46:05 -0300

    estudar

--- Nova pasta/src/InverterString.js
+++
@@ -1,8 +0,0 @@
-function InverterStringPilha(str){
-    const stack = [];
-    stack.push(...str)
-    stack.reverse()
-    console.log(stack)
-}
-export default InverterStringPilha;
-InverterStringPilha('Eduarda')
--- Nova pasta/tests/ehPositivo.test.js
+++
@@ -1,9 +0,0 @@
-import { Negativo, Positivo } from "../src/positivo.js";
-
-
-test("confirmação de número positivo", () => {
-    expect(Positivo).toBe(1);
-  });
-test("confirmação de número positivo", () => {
-    expect(Negativo((-12))).toBe(-1);
-  });
