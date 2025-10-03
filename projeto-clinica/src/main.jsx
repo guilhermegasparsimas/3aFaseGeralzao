690154e80f287c5f67c62784b49c3cdb896f16ca
guilhermegasparsimas <guisimas0201@gmail.com>
2025-10-02 22:08:50 -0300

    'Projeto'

---
+++ projeto-clinica/src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import { AuthProvider } from './contexts/AuthContext';
+// import App from './App.jsx' @@Retiramos esta importação pois não estamos mais utilizando o css desta página, estamos usando tailwindcss, e no arquivo index.css importamos o @ import "tailwindcss" para utilizar de modo GLOBAL neste projeto
+
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <AuthProvider>
+      <ToastContainer />
+      <RouterProvider router={router}/>
+
+    </AuthProvider>
+
+
+  </StrictMode>,
+)
---
+++ projeto-clinica/src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import Logo from '../../assets/Logo.png';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+export default function Login(){
+    return <>
+        <div className='flex min-h-screen bg-gray-100'>
+            <div className='hidden md:flex w-1/2 bg-gray-200 flew-col items-center justify-center p-8'>
+                <img src={Logo} alt="Clinica Médica" className='mb-6'/>
+            </div>
+            <div className='flex w-full md:-1/2 items-center justify-center p-8'>
+                <LoginForm />                
+            </div>
+        </div>
+    </>
+}
